6e790c769000c7fc62a3c36a7aaec05f4de3a014
dariosalvi78 <dariosalvi78@f5fb6dda-4b9d-3bba-4afb-d541c8323973>
2010-04-12 21:51:17 +0000

    Adding some comments to WiiManager

--- trunk/Java/WiiManager/src/bochoVJ/wii/DummyWiiHandler.java
+++ trunk/Java/WiiManager/src/bochoVJ/wii/DummyWiiHandler.java
@@ -8,21 +8,20 @@
 package bochoVJ.wii;
 
 /**
- * 
+ * A simple WiiMote handler, just prints the events on console
  * @author bochovj
  *
  */
 public class DummyWiiHandler implements IWiiHandler {
 
-	@Override
-	public void handleAcc(Acceleration acc) {
-		System.out.println("Acceleration: x "+acc.x+" y "+acc.y+" z "+acc.z);
+    @Override
+    public void handleAcc(Acceleration acc) {
+	System.out.println("Acceleration: x "+acc.x+" y "+acc.y+" z "+acc.z);
 
-	}
+    }
 
-	@Override
-	public void handleButton(WiiButton b) {
-		System.out.println("Button "+b.toString());
-	}
-
+    @Override
+    public void handleButton(WiiButton b) {
+	System.out.println("Button "+b.toString());
+    }
 }
