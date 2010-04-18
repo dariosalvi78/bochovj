48510aa5ce39a0c08ec1ceedee3eab95512aae52
dariosalvi78 <dariosalvi78@f5fb6dda-4b9d-3bba-4afb-d541c8323973>
2010-04-18 13:48:43 +0000

    if there's no webcam, no pain little bug fix because of refactoring

--- Java/MidiDraw/src/bochovj/midiDraw/Drawer.java
+++ Java/MidiDraw/src/bochovj/midiDraw/Drawer.java
@@ -8,10 +8,17 @@ public class Drawer {
 	//Video capturing must be initialized outside the Applet
 	//Or it won't ever get permissions in Windows Vista
 	DrawerApplet.videograbber = new VideoGrabber();
-	DrawerApplet.videograbber.startCapture();
+	try{
+	    DrawerApplet.videograbber.startCapture();
+	}
+	catch(Exception ex)
+	{
+	    System.out.println("Could not open capture device, yuou won't be able to use it");
+	}
+	
 	
 	//Starts the applet in full screen
-	PApplet.main(new String[] { "--present", "bochovj.midiDraw.Drawer" });
+	PApplet.main(new String[] { "--present", "bochovj.midiDraw.DrawerApplet" });
     }
 
 }
