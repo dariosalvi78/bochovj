bd97be3508bc7db6045d0612de1f4dff5c314524
dariosalvi78 <dariosalvi78@f5fb6dda-4b9d-3bba-4afb-d541c8323973>
2010-06-10 22:39:44 +0000

    Some improvements in the WiiMovingPoint visualizer

--- trunk/Java/Visualizers/src/bochoVJ/wii/applets/WiiMovingPoint.java
+++ trunk/Java/Visualizers/src/bochoVJ/wii/applets/WiiMovingPoint.java
@@ -1,81 +1,139 @@
+/**
+ * Visualizers for WiiManager
+ * 
+ * Distributed under artistic license:
+ * http://www.opensource.org/licenses/artistic-license-2.0.php
+ * 
+ */
 package bochoVJ.wii.applets;
 
 
+import java.awt.Color;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
 import bochoVJ.wii.IWiiHandler;
 import bochoVJ.wii.WiiManager;
+import bochovj.draw.Point;
 import processing.core.PApplet;
 
+/**
+ * A drawer for the WiiMote
+ * @author bochovj
+ *
+ */
 public class WiiMovingPoint extends PApplet {
-	
-	WiiManager wiimng;
-	
-	private final int xSize = 400;
-	private final int ySize = 400;
-	
-	private float xspeed = 0, yspeed = 0;
-	private float xpos = 200, ypos = 200;
-	
-	private float accFactor = 1;
-	
-	public static void main(String args[]) {
-	    PApplet.main(new String[] { "--present", WiiMovingPoint.class.getName() });
-	  }
-	
-	public void setup()
-	{	
-		wiimng = new WiiManager();
-		wiimng.addHandler(new IWiiHandler() {
-			
-			@Override
-			public void handleButton(WiiButton b) {
-			}
-			
-			@Override
-			public void handleAcc(Acceleration acc) {
-				xspeed = (float)(acc.x * accFactor);
-				yspeed = (float)(acc.y * accFactor);
-			}
-		});
-		
-		size(xSize, ySize); 
-		background(0);
-	}
 
-	public void draw(){
-		background(0);
+    WiiManager wiimng;
+
+    private final int xSize = 600;
+    private final int ySize = 600;
+
+    private float xspeed = 0, yspeed = 0;
+    private float xpos = xSize/2, ypos = ySize/2;
+
+    private float size =1;
+    private final float maxSize = 10;
+    
+    private Color currentColor;
+
+    private float accFactor = 2;
+
+    List<Point> points = new LinkedList<Point>();
+
+    public static void main(String args[]) {
+	PApplet.main(new String[] { "--present", WiiMovingPoint.class.getName() });
+    }
+
+    public void setup()
+    {	
+	wiimng = new WiiManager();
+	wiimng.addHandler(new IWiiHandler() {
+
+	    @Override
+	    public void handleButton(WiiButton bt) {
 		
-		xpos += xspeed;
-		if(xpos < 0)
-			xpos = xSize;
-		if(ypos > xSize)
-			xpos =0;
-		
-		ypos += yspeed;
-		if(ypos < 0)
-			ypos = ySize;
-		if(ypos > ySize)
-			ypos =0;
-		
-		fill(250);
-		stroke(250);
-		ellipse(xpos, ypos, 5, 5);
+	    }
+
+	    @Override
+	    public void handleAcc(Acceleration acc) {
+		xspeed = (float)(acc.x * accFactor);
+		yspeed = (float)(acc.y * accFactor);
+		size = abs(((float)acc.z / 5) * maxSize);
+		currentColor = new Color((float)map(acc.x, -5, 5, 0, 255), 
+			(float)map(acc.y, -5, 5, 0, 255), 
+			(float)map(acc.z, -5, 5, 0, 255));
+	    }
+	});
+
+	size(xSize, ySize); 
+	background(0);
+	currentColor = new Color(250,250,250);
+    }
+
+    public void draw(){
+	background(0);
+	strokeJoin(ROUND);
+	strokeCap(ROUND);
+
+	xpos += xspeed;
+	if(xpos < 0)
+	    xpos = 0;
+	if(xpos > xSize)
+	    xpos = xSize;
+
+	ypos += yspeed;
+	if(ypos < 0)
+	    ypos = 0;
+	if(ypos > ySize)
+	    ypos = ySize;
+
+	if(points.size() ==0)
+	    points.add(new Point(xpos, ypos, currentColor, abs( xspeed) + abs(yspeed)));
+	else
+	{
+	    Point p = points.get(0);
+	    if(!((p.x == xpos) || (p.y == ypos)))
+		points.add(new Point(xpos, ypos, currentColor, size));	
 	}
 	
-	public void keyTyped() {
-		  if(key=='c')
-			try {
-				wiimng.connect();
-			} catch (Exception e) {
-				e.printStackTrace();
-			} 
-		  
-		  if(key=='d')
-			  wiimng.disconnect();
-		  
-		  if(key =='+')
-			  accFactor += 0.5;
-		  
-		  if(key =='-')
-			  accFactor -= 0.5;
-		}
+	points.add(new Point(xpos, ypos, currentColor, abs( xspeed) + abs(yspeed)));
+	
+	smooth();
+	if(points.size()>1)
+	{
+	    Iterator<Point> piter = points.iterator();
+	    Point previousPoint = piter.next();
+	    while(piter.hasNext())
+	    {
+		Point nextPoint = piter.next();
+		strokeWeight(nextPoint.weight);
+		stroke(nextPoint.color.getRed(),nextPoint.color.getGreen(),nextPoint.color.getBlue(),100);
+		line(previousPoint.x, previousPoint.y, nextPoint.x, nextPoint.y);
+		previousPoint = nextPoint;
+	    }
+	}
+	noSmooth();
+
+    }
+
+    public void keyTyped() {
+	if(key=='c')
+	    try {
+		wiimng.connect();
+	    } catch (Exception e) {
+		e.printStackTrace();
+	    } 
+
+	    if(key=='d')
+		wiimng.disconnect();
+
+	    if(key =='+')
+		accFactor += 0.5;
+
+	    if(key =='-')
+		accFactor -= 0.5;
+    }
 }
--- trunk/Java/Visualizers/src/bochoVJ/wii/applets/WiiTester.java
+++ trunk/Java/Visualizers/src/bochoVJ/wii/applets/WiiTester.java
@@ -7,95 +7,95 @@ import processing.core.PApplet;
 import processing.core.PFont;
 
 public class WiiTester extends PApplet {
-	
-	WiiManager wiimng;
-	
-	private final int xSize = 400;
-	private final int ySize = 400;
-	
-	private int[] accX = new int[xSize];
-	private int accXPointer = 0;
-	
-	private int[] accY = new int[xSize];
-	private int accYPointer = 0;
-	
-	private int[] accZ = new int[xSize];
-	private int accZPointer = 0;
-	
-	public static void main(String args[]) {
-	    PApplet.main(new String[] { "--present", WiiTester.class.getName() });
-	  }
-	
-	public void setup()
-	{	
-		wiimng = new WiiManager();
-		wiimng.addHandler(new IWiiHandler() {
-			
-			@Override
-			public void handleButton(WiiButton b) {
-				
-			}
-			
-			@Override
-			public void handleAcc(Acceleration acc) {
-				accX[accXPointer] = (int)(acc.x * 10);
-				accXPointer ++;
-				if(accXPointer >= xSize)
-					accXPointer = 0;
-				
-				accY[accYPointer] = (int)(acc.y * 10);
-				accYPointer ++;
-				if(accYPointer >= xSize)
-					accYPointer = 0;
-				
-				accZ[accZPointer] = (int)(acc.z * 10);
-				accZPointer ++;
-				if(accZPointer >= xSize)
-					accZPointer = 0;
-			}
-		});
-		
-		size(xSize, ySize); 
-		background(0);
+
+    WiiManager wiimng;
+
+    private final int xSize = 400;
+    private final int ySize = 400;
+
+    private int[] accX = new int[xSize];
+    private int accXPointer = 0;
+
+    private int[] accY = new int[xSize];
+    private int accYPointer = 0;
+
+    private int[] accZ = new int[xSize];
+    private int accZPointer = 0;
+
+    public static void main(String args[]) {
+	PApplet.main(new String[] { "--present", WiiTester.class.getName() });
+    }
+
+    public void setup()
+    {	
+	wiimng = new WiiManager();
+	wiimng.addHandler(new IWiiHandler() {
+
+	    @Override
+	    public void handleButton(WiiButton b) {
+
+	    }
+
+	    @Override
+	    public void handleAcc(Acceleration acc) {
+		accX[accXPointer] = (int)(acc.x * 10);
+		accXPointer ++;
+		if(accXPointer >= xSize)
+		    accXPointer = 0;
+
+		accY[accYPointer] = (int)(acc.y * 10);
+		accYPointer ++;
+		if(accYPointer >= xSize)
+		    accYPointer = 0;
+
+		accZ[accZPointer] = (int)(acc.z * 10);
+		accZPointer ++;
+		if(accZPointer >= xSize)
+		    accZPointer = 0;
+	    }
+	});
+
+	size(xSize, ySize); 
+	background(0);
+    }
+
+    public void draw(){
+	background(0);
+
+	fill(0, 102, 153);
+	PFont font;
+	font = loadFont("CourierNew36.vlw"); 
+	textFont(font, 15); 
+	text("Press c to connect, d to disconnect", 10, 20);
+
+	stroke(250);
+	for (int i=0; i< xSize; i++)
+	{
+	    point(i, accX[i]+200);
 	}
 
-	public void draw(){
-		background(0);
-		
-		fill(0, 102, 153);
-		PFont font;
-		font = loadFont("CourierNew36.vlw"); 
-		textFont(font, 15); 
-		text("Press c to connect, d to disconnect", 10, 20);
-		
-		stroke(250);
-		for (int i=0; i< xSize; i++)
-		{
-			point(i, accX[i]+200);
-		}
-		
-		stroke(250, 0, 0);
-		for (int i=0; i< xSize; i++)
-		{
-			point(i, accY[i]+200);
-		}
-		
-		stroke(0, 250, 0);
-		for (int i=0; i< xSize; i++)
-		{
-			point(i, accZ[i]+200);
-		}
+	stroke(250, 0, 0);
+	for (int i=0; i< xSize; i++)
+	{
+	    point(i, accY[i]+200);
 	}
-	
-	public void keyTyped() {
-		  if(key=='c')
-			try {
-				wiimng.connect();
-			} catch (Exception e) {
-				e.printStackTrace();
-			} 
-		  
-		  if(key=='d')
-			  wiimng.disconnect();
-		}
+
+	stroke(0, 250, 0);
+	for (int i=0; i< xSize; i++)
+	{
+	    point(i, accZ[i]+200);
+	}
+    }
+
+    public void keyTyped() {
+	if(key=='c')
+	    try {
+		wiimng.connect();
+	    } catch (Exception e) {
+		e.printStackTrace();
+	    } 
+
+	    if(key=='d')
+		wiimng.disconnect();
+    }
 }
