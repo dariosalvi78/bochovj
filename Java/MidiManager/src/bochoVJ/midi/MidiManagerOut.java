a5ba4eab2bf35ef88a4c9104292dc70a3e393d99
dariosalvi78@gmail.com <dariosalvi78@gmail.com@f5fb6dda-4b9d-3bba-4afb-d541c8323973>
2011-12-08 16:08:35 +0000

    Adding serial to midi, a tool that converts simple messages read from serial port to midi messages

--- Java/MidiManager/src/bochoVJ/midi/MidiManagerOut.java
+++ Java/MidiManager/src/bochoVJ/midi/MidiManagerOut.java
@@ -25,144 +25,144 @@ import javax.sound.midi.Transmitter;
  */
 public class MidiManagerOut implements Transmitter{
 
-    boolean started;
+	boolean started;
 
-    Receiver receiver;
+	Receiver receiver;
 
-    public MidiManagerOut()
-    {
-	started = false;
-    }
-
-    @Override
-    public Receiver getReceiver() {
-	return this.receiver;
-    }
-
-    @Override
-    public void setReceiver(Receiver receiver) {
-	this.receiver = receiver;
-    }
-
-    public void sendControlChange(int channel, int controller, int value) throws InvalidMidiDataException 
-    {
-	ShortMessage msg = new ShortMessage();
-	msg.setMessage(ShortMessage.CONTROL_CHANGE, channel, controller, value);
-	this.receiver.send(msg, -1);
-    }
-
-    public void sendNoteOn(int channel, int note, int velocity) throws InvalidMidiDataException
-    {
-	ShortMessage msg = new ShortMessage();
-	msg.setMessage(ShortMessage.NOTE_ON, channel, note, velocity);
-	this.receiver.send(msg, -1);
-    }
-
-    public void startDevice(int deviceNumber) throws Exception
-    {
-	if(started == true)
-	    throw new Exception("Cannot start an already started device, first close it");
-	
-	MidiDevice.Info[] aInfos = MidiSystem.getMidiDeviceInfo();
-	System.out.println("Opening OUT device: "+aInfos[deviceNumber].getName());
-	
-	MidiDevice device = MidiSystem.getMidiDevice(aInfos[deviceNumber]);
-	device.open();
-	Receiver r = device.getReceiver();
-
-	this.setReceiver(r);	
-	started = true;
-    }
-
-    public void startEmulation() throws Exception
-    {
-	if(started == true)
-	    throw new Exception("Cannot start an already started device, first close it");
-
-	new Thread(new Runnable() {
-	    @Override
-	    public void run() {
-		started = true;
-		while(started)
-		{
-		    try 
-		    {
-			for(int i = 0; i<128; i++)
-			{
-			    //Send note
-			    int note = new Random().nextInt(127);
-			    int intensity = new Random().nextInt(127);
-			    sendNoteOn(1, note, intensity);
-			    //Send control
-			    sendControlChange(1, 16, i);
-			    Thread.sleep(1000);
-			}
-		    } 
-		    catch (Exception e) 
-		    {
-			e.printStackTrace();
-		    }
-		}
-	    }
-	}).start();
-    }
-
-    @Override
-    public void close() {
-	receiver.close();
-	started = false;
-    }
-
-    public static int promptUserSelectDevice()
-    {
-	LinkedList<MidiDeviceSelectDialog.MidiDevice> devices = new LinkedList<MidiDeviceSelectDialog.MidiDevice>();
-	MidiDevice.Info[] aInfos = MidiSystem.getMidiDeviceInfo();
-	for (int i = 0; i < aInfos.length; i++) {
-	    try {
-		MidiDevice device = MidiSystem.getMidiDevice(aInfos[i]);
-		if(device.getMaxReceivers() != 0)
-		{
-		    devices.add(new MidiDeviceSelectDialog.MidiDevice(i, aInfos[i].getName()));
-		    System.out.println("" + i + "  "
-			    + aInfos[i].getName() + ", " + aInfos[i].getVendor()
-			    + ", " + aInfos[i].getVersion() + ", "
-			    + aInfos[i].getDescription());
-		}
-	    }  
-	    catch (MidiUnavailableException e) {
-		//Ignore device
-	    }
+	public MidiManagerOut()
+	{
+		started = false;
 	}
 
-	//Generate dialog:
-	MidiDeviceSelectDialog dial = new MidiDeviceSelectDialog(null, "Please select Midi OUT device");
-	dial.createMidiOutList(devices);
-	dial.setDeviceHandler(new MidiDeviceSelectDialog.IDeviceHandler() {
-	    @Override
-	    public void handle(int deviceNumber) {
-		devN = deviceNumber;
+	@Override
+	public Receiver getReceiver() {
+		return this.receiver;
+	}
+
+	@Override
+	public void setReceiver(Receiver receiver) {
+		this.receiver = receiver;
+	}
+
+	public void sendControlChange(int channel, int controller, int value) throws InvalidMidiDataException 
+	{
+		ShortMessage msg = new ShortMessage();
+		msg.setMessage(ShortMessage.CONTROL_CHANGE, channel, controller, value);
+		this.receiver.send(msg, -1);
+	}
+
+	public void sendNoteOn(int channel, int note, int velocity) throws InvalidMidiDataException
+	{
+		ShortMessage msg = new ShortMessage();
+		msg.setMessage(ShortMessage.NOTE_ON, channel, note, velocity);
+		this.receiver.send(msg, -1);
+	}
+
+	public void startDevice(int deviceNumber) throws Exception
+	{
+		if(started == true)
+			throw new Exception("Cannot start an already started device, first close it");
+
+		MidiDevice.Info[] aInfos = MidiSystem.getMidiDeviceInfo();
+		System.out.println("Opening OUT device: "+aInfos[deviceNumber].getName());
+
+		MidiDevice device = MidiSystem.getMidiDevice(aInfos[deviceNumber]);
+		device.open();
+		Receiver r = device.getReceiver();
+
+		this.setReceiver(r);	
+		started = true;
+	}
+
+	public void startEmulation() throws Exception
+	{
+		if(started == true)
+			throw new Exception("Cannot start an already started device, first close it");
+
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				started = true;
+				while(started)
+				{
+					try 
+					{
+						for(int i = 0; i<128; i++)
+						{
+							//Send note
+							int note = new Random().nextInt(127);
+							int intensity = new Random().nextInt(127);
+							sendNoteOn(1, note, intensity);
+							//Send control
+							sendControlChange(1, 16, i);
+							Thread.sleep(1000);
+						}
+					} 
+					catch (Exception e) 
+					{
+						e.printStackTrace();
+					}
+				}
+			}
+		}).start();
+	}
+
+	@Override
+	public void close() {
+		receiver.close();
+		started = false;
+	}
+
+	public static int promptUserSelectDevice()
+	{
+		LinkedList<MidiDeviceSelectDialog.MidiDevice> devices = new LinkedList<MidiDeviceSelectDialog.MidiDevice>();
+		MidiDevice.Info[] aInfos = MidiSystem.getMidiDeviceInfo();
+		for (int i = 0; i < aInfos.length; i++) {
+			try {
+				MidiDevice device = MidiSystem.getMidiDevice(aInfos[i]);
+				if(device.getMaxReceivers() != 0)
+				{
+					devices.add(new MidiDeviceSelectDialog.MidiDevice(i, aInfos[i].getName()));
+					System.out.println("" + i + "  "
+							+ aInfos[i].getName() + ", " + aInfos[i].getVendor()
+							+ ", " + aInfos[i].getVersion() + ", "
+							+ aInfos[i].getDescription());
+				}
+			}  
+			catch (MidiUnavailableException e) {
+				//Ignore device
+			}
+		}
+
+		//Generate dialog:
+		MidiDeviceSelectDialog dial = new MidiDeviceSelectDialog(null, "Please select Midi OUT device");
+		dial.createMidiOutList(devices);
+		dial.setDeviceHandler(new MidiDeviceSelectDialog.IDeviceHandler() {
+			@Override
+			public void handle(int deviceNumber) {
+				devN = deviceNumber;
+				synchronized(mutex)
+				{
+					mutex.notify();
+				}
+			}
+		});
+		dial.setVisible(true);
 		synchronized(mutex)
 		{
-		    mutex.notify();
+			try {
+				mutex.wait();
+			} 
+			catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
-	    }
-	});
-	//dial.setEnabled(true);
-	dial.setVisible(true);
-	synchronized(mutex)
-	{
-	    try {
-		mutex.wait();
-	    } 
-	    catch (InterruptedException e) {
-		e.printStackTrace();
-	    }
+		dial.dispose();
+
+		return devN;
 	}
 
-	return devN;
-    }
-
-    private static int devN;
-    private static Object mutex = new Object();
+	private static int devN;
+	private static Object mutex = new Object();
 }
 
---
+++ Java/MidiManager/src/bochoVJ/midi/serial/SerialToMidi.java
@@ -0,0 +1,177 @@
+/**
+ * SerialToMidi is a simple application that gets messages from a simple protocol
+ * on the serial port and routes them to the midi system.
+ * The protocol specification is:
+ * <ul>
+ * <li> one message per line (i.e. packets are separated by a carriage return)
+ * <li> Control changes are represented as: cc ch 10 ctrl 12 val 130
+ * <li> Notes ON are represented as: non ch 10 key 12 vel 120
+ * <li> Notes OFF are represented as: noff ch 10 key 14 vel 120
+ * <li> Aftertouches are represented as: at ch 10 key 14 tch 100
+ * </ul>
+ * 
+ * Distributed under artistic license:
+ * http://www.opensource.org/licenses/artistic-license-2.0.php
+ * 
+ */
+package bochoVJ.midi.serial;
+
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowStateListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+import javax.sound.midi.InvalidMidiDataException;
+import javax.swing.JOptionPane;
+
+import bochoVJ.midi.MidiManagerOut;
+
+import gnu.io.*;
+
+/**
+ * @author bochovj
+ *
+ */
+public class SerialToMidi {
+
+	private static Logger logger = Logger.getLogger(SerialToMidi.class.getName());
+	MidiManagerOut midiout;
+	int midioutN;
+	CommPortIdentifier portId;
+	CommPort port;
+	boolean keepreading;
+
+
+	public SerialToMidi(String port, int midiout) throws Exception
+	{
+		portId =CommPortIdentifier.getPortIdentifier(port);
+		if(portId == null)
+		{
+			throw new Exception("Cannot open serial port "+port);
+		}
+		this.midioutN = midiout;
+		this.midiout = new MidiManagerOut();
+	}
+
+	/**
+	 * Lists available serial ports.
+	 * @return
+	 */
+	public static List<String> getSerialPorts()
+	{
+		List<String> retVal = new ArrayList<String>();
+		Enumeration<CommPortIdentifier> ports = CommPortIdentifier.getPortIdentifiers();
+		while(ports.hasMoreElements())
+		{
+			CommPortIdentifier port = ports.nextElement();
+			if(port.getPortType() == CommPortIdentifier.PORT_SERIAL)
+			{
+				retVal.add(port.getName());
+			}
+		}
+
+		return retVal;
+	}
+
+	public void start() throws Exception
+	{
+		midiout.startDevice(midioutN);
+
+		port = (SerialPort) portId.open(
+				"BochoVJ", // Name of the application asking for the port 
+				10000   // Wait max. 10 sec. to acquire port
+				);
+
+		InputStream is= port.getInputStream();
+		final BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+		keepreading = true;
+
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				while(keepreading)
+				{
+					String line = null;
+					try {
+						line = reader.readLine();
+						String[] data = line.split(" ");
+						//cc ch 10 ctrl 12 val 130
+						if(data[0].equalsIgnoreCase("cc"))
+						{
+							int channel = Integer.parseInt(data[2]);
+							int ctrl = Integer.parseInt(data[4]);
+							int val = Integer.parseInt(data[6]);
+							midiout.sendControlChange(channel, ctrl, val);
+						}
+						else if(data[0].equalsIgnoreCase("non"))
+						{
+							int channel = Integer.parseInt(data[2]);
+							int key = Integer.parseInt(data[4]);
+							int vel = Integer.parseInt(data[6]);
+							midiout.sendNoteOn(channel, key, vel);
+						}
+					} catch (IOException e) {
+
+					}catch(InvalidMidiDataException ie)
+					{
+						System.out.println("error");
+					}
+				}
+			}
+		}).start();
+
+	}
+
+
+	public void stop()
+	{
+		keepreading = false;
+		port.close();
+		midiout.close();
+	}
+
+
+	public static void main(String[] args)
+	{
+		try{
+			FileHandler fh = new FileHandler("serialToMidi.log");
+			fh.setFormatter(new SimpleFormatter());
+			logger.addHandler(fh);
+			logger.setLevel(Level.ALL);
+
+			String chosenPort = (String) JOptionPane.showInputDialog(null, "Select serial port",
+					"Serial to Midi",
+					JOptionPane.QUESTION_MESSAGE, 
+					null, getSerialPorts().toArray(), null);
+
+			logger.info("Chosen serial port "+chosenPort);
+
+			int out = MidiManagerOut.promptUserSelectDevice();
+
+			logger.info("Chosen midi port "+out);
+
+			SerialToMidi sm = new SerialToMidi(chosenPort, out);
+			SerialToMidiDialog dialog = new SerialToMidiDialog(sm);
+			dialog.setVisible(true);
+		}
+		catch (Exception e) {
+			logger.severe("Error: "+e.getMessage());
+			System.exit(-1);
+		}
+
+	}
+
+}
