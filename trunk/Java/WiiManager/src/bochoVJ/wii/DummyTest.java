e4aa1df033b5bc8d84362c930101c1f2dfdd922e
dariosalvi78@gmail.com <dariosalvi78@gmail.com@f5fb6dda-4b9d-3bba-4afb-d541c8323973>
2012-01-27 10:56:15 +0000

    Abstracting WiiManager, and added a WiiManager for the wiiusej library

--- trunk/Java/WiiManager/src/bochoVJ/wii/DummyTest.java
+++ trunk/Java/WiiManager/src/bochoVJ/wii/DummyTest.java
@@ -7,26 +7,55 @@
  */
 package bochoVJ.wii;
 
+
 /**
- * A simple test for the WiiManager
+ * A simple test for the WiiManager.
  * @author bochovj
  *
  */
 public class DummyTest {
 
-    public static void main(String[] args) throws Exception
-    {
-	WiiManager mng = new WiiManager();
-	DummyWiiHandler handler = new DummyWiiHandler();
-	mng.addHandler(handler);
+	/**
+	 * A simple WiiMote handler, just prints the events on console
+	 * @author bochovj
+	 *
+	 */
+	public static class DummyWiiHandler implements IWiiHandler {
 
-	mng.connect();
+		public double max;
 
-	Thread.sleep(60000);
+		@Override
+		public void handleAcc(Acceleration acc) {
+			//System.out.println("Acceleration: x "+acc.x+" y "+acc.y+" z "+acc.z);
+			if(acc.x > max)
+				max = acc.x;
+			if(acc.y > max)
+				max = acc.y;
+			if(acc.z > max)
+				max = acc.z;
+		}
 
-	mng.disconnect();
-	
-	System.out.println("Max: "+handler.max);
-    }
+		@Override
+		public void handleButton(WiiButton b) {
+			System.out.println("Button "+b.toString());
+		}
+	}
+
+	public static void main(String[] args) throws Exception
+	{
+		IWiiManager mng = new WiiuseJManager();
+		DummyWiiHandler handler = new DummyWiiHandler();
+		mng.addHandler(handler);
+
+		mng.connect();
+
+		Thread.sleep(60000);
+
+		mng.disconnect();
+
+		System.out.println("Max: "+handler.max);
+	}
 
 }
+
+
