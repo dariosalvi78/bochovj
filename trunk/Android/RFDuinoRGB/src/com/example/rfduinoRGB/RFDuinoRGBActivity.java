d0f3fbf51b92f467735b48564578c932f670e51d
dariosalvi78@gmail.com <dariosalvi78@gmail.com@f5fb6dda-4b9d-3bba-4afb-d541c8323973>
2013-12-25 14:45:09 +0000

    Improved comments and better reliability

--- trunk/Android/RFDuinoRGB/src/com/example/rfduinoRGB/RFDuinoRGBActivity.java
+++ trunk/Android/RFDuinoRGB/src/com/example/rfduinoRGB/RFDuinoRGBActivity.java
@@ -25,7 +25,6 @@
 package com.example.rfduinoRGB;
 
 
-import java.util.List;
 import java.util.UUID;
 
 import android.os.Bundle;
@@ -37,7 +36,6 @@ import android.bluetooth.BluetoothGattCallback;
 import android.bluetooth.BluetoothAdapter.LeScanCallback;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothGattCharacteristic;
-import android.bluetooth.BluetoothGattDescriptor;
 import android.bluetooth.BluetoothGattService;
 import android.bluetooth.BluetoothProfile;
 import android.util.Log;
@@ -48,13 +46,20 @@ import android.widget.SeekBar.OnSeekBarChangeListener;
 
 import com.example.rfduinoRGB.R;
 
-
+/**
+ * A simple Android application for controlling an RGB LED on the
+ * RFduino (see http://www.rfduino.com/) with the ColorWheel sketch
+ * (see https://github.com/RFduino/RFduino/tree/master/libraries/RFduinoBLE/examples/ColorWheel)
+ * @author Dario Salvi
+ *
+ */
 public class RFDuinoRGBActivity extends Activity {
 
 	/**
 	 * Button used to connect to the RFDuino
 	 */
 	private Button connectButton;
+	
 	/**
 	 * RGB bars.
 	 */
@@ -77,24 +82,31 @@ public class RFDuinoRGBActivity extends Activity {
 
 	/**
 	 * Service where to send data.
+	 * Discovered empirically,
+	 * it would be possible to find it programatically by parsing the GATT messages.
 	 */
 	private final String serviceUUID = "00002220-0000-1000-8000-00805f9b34fb";
 
 	/**
 	 * Characteristic where to send data.
+	 * Discovered empirically,
+	 * it would be possible to find it programatically by parsing the GATT messages.
 	 */
 	private final String sendCharacteristicUUID = "00002222-0000-1000-8000-00805f9b34fb";
 
 	/**
-	 * Timeout for scanning.
+	 * Timeout for searching for an RFduino.
 	 */
-	private static final long SCAN_PERIOD = 10000;
+	private static final long SCAN_PERIOD = 20000;
 
 	/**
 	 * Used to call functions asynchronously.
 	 */
 	private Handler handler;
 
+	/**
+	 * Tells if we are connected or not.
+	 */
 	private boolean connected;
 
 	/**
@@ -121,17 +133,19 @@ public class RFDuinoRGBActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				if(!connected){
+					//If not connected, let's look for RFduino and connect to it
 					connectButton.setEnabled(false);
 					bluetoothAdapter.startLeScan(handleScan);
-
-					//Stop after some seconds
+					
+					//Program a to stop the scanning after some seconds
 					handler.postDelayed(new Runnable() {
 						@Override
 						public void run() {
 							if(!connected){
+								//Cancel scan only if not connected
 								Log.d(TAG, "Scanning timeout, stopping it");
-								//Cancel scan
 								bluetoothAdapter.stopLeScan(handleScan);
+								//All graphical things must be run on the UI thread:
 								RFDuinoRGBActivity.this.runOnUiThread(new Runnable() {
 									@Override
 									public void run() {
@@ -144,10 +158,11 @@ public class RFDuinoRGBActivity extends Activity {
 						}
 					}, SCAN_PERIOD);
 
-				}else {
+				} else {
+					//If connected, the same button is used for disconnecting
 					connected = false;
 					connectButton.setText(R.string.connect);
-
+					
 					redBar.setEnabled(false);
 					greenBar.setEnabled(false);
 					blueBar.setEnabled(false);
@@ -160,7 +175,7 @@ public class RFDuinoRGBActivity extends Activity {
 
 		redBar = (SeekBar) findViewById(R.id.redBar);
 		redBar.setEnabled(false);
-		redBar.setMax(255);
+		redBar.setMax(255);//RGB are sent as 1 byte each, values from 0 to 255
 		redBar.setOnSeekBarChangeListener(barchangeListener);
 
 		greenBar = (SeekBar) findViewById(R.id.greenBar);
@@ -173,14 +188,9 @@ public class RFDuinoRGBActivity extends Activity {
 		blueBar.setMax(255);
 		blueBar.setOnSeekBarChangeListener(barchangeListener);
 	}
-
-	@Override
-	protected void onStart() {
-		super.onStart();
-	}
 	
 	OnSeekBarChangeListener barchangeListener = new OnSeekBarChangeListener() {
-
+		
 		@Override
 		public void onStopTrackingTouch(SeekBar seekBar) {
 		}
@@ -193,40 +203,38 @@ public class RFDuinoRGBActivity extends Activity {
 		@Override
 		public void onProgressChanged(SeekBar seekBar, int progress,
 				boolean fromUser) {
+			//When the value of a bar changes, send the values to RFduino
 			sendColor();
 		}
 	};
 
+	/**
+	 * Sends the color to RFduino.
+	 * Reads the values in the seekbars and sends them as 3 bytes.
+	 */
 	private void sendColor(){
 		byte[] data = new byte[3];
 		data[0] = (byte) redBar.getProgress();
 		data[1] = (byte) greenBar.getProgress();
 		data[2] = (byte) blueBar.getProgress();
-		sendCharacteristic.setValue(data);
-		sendCharacteristic.setWriteType(BluetoothGattCharacteristic.WRITE_TYPE_NO_RESPONSE);
-		bluetoothGatt.writeCharacteristic(sendCharacteristic);
+		if(sendCharacteristic != null){
+			sendCharacteristic.setValue(data);
+			sendCharacteristic.setWriteType(BluetoothGattCharacteristic.WRITE_TYPE_NO_RESPONSE);
+			bluetoothGatt.writeCharacteristic(sendCharacteristic);
+		}
 	}
 
+	/**
+	 * Handles the scanning of BLE devices.
+	 */
 	LeScanCallback handleScan = new LeScanCallback() {
 		@Override
 		public void onLeScan(final BluetoothDevice device, int rssi, byte[] scanRecord) {
 			Log.d(TAG, "Found BTLE device: " + device.getName());
 			if(device.getName().equalsIgnoreCase("RFduino")){
-				//Got it!
+				//Got a RFduino !
 				connected = true;
 				bluetoothAdapter.stopLeScan(handleScan);
-
-				RFDuinoRGBActivity.this.runOnUiThread(new Runnable() {
-					@Override
-					public void run() {
-						connectButton.setEnabled(true);
-						connectButton.setText(R.string.disconnect);
-
-						redBar.setEnabled(true);
-						greenBar.setEnabled(true);
-						blueBar.setEnabled(true);
-					}
-				});
 
 				Log.d(TAG, "Found RFDuino trying to connect to " + device.getAddress());
 				bluetoothGatt = device.connectGatt(RFDuinoRGBActivity.this, true, new BluetoothGattCallback() {
@@ -244,10 +252,25 @@ public class RFDuinoRGBActivity extends Activity {
 					@Override
 					public void onServicesDiscovered(BluetoothGatt gatt, int status) {
 
-						if (status == BluetoothGatt.GATT_SUCCESS) {
-							printServicesList();
+						if (status == BluetoothGatt.GATT_SUCCESS) {						
+							//Once the services have been discovered we can connect to the one
+							//we know is used for receiving the RGB values
 							BluetoothGattService serv = bluetoothGatt.getService(UUID.fromString(serviceUUID));
 							sendCharacteristic = serv.getCharacteristic(UUID.fromString(sendCharacteristicUUID));
+							
+							//Now we assume that the device is fully connected
+							RFDuinoRGBActivity.this.runOnUiThread(new Runnable() {
+								@Override
+								public void run() {
+									//On the UI we shall enable the bars and the disconnect button
+									connectButton.setEnabled(true);
+									connectButton.setText(R.string.disconnect);
+
+									redBar.setEnabled(true);
+									greenBar.setEnabled(true);
+									blueBar.setEnabled(true);
+								}
+							});
 						}
 					}
 				});
@@ -255,31 +278,10 @@ public class RFDuinoRGBActivity extends Activity {
 		}
 	};
 
-	private void printServicesList(){
-		List<BluetoothGattService> services = bluetoothGatt.getServices();
-		for(BluetoothGattService ser : services){
-			Log.d(TAG, "Discovered service UUID: "+ser.getUuid().toString());
-			List<BluetoothGattCharacteristic> characteristics = ser.getCharacteristics();
-			for(BluetoothGattCharacteristic charac : characteristics){
-				Log.d(TAG, "Got characteristic UUID: " + charac.getUuid() + 
-						" properties: " + charac.getProperties() +
-						" permissions: " + charac.getPermissions());
-				List<BluetoothGattDescriptor> descrs = charac.getDescriptors();
-				for(BluetoothGattDescriptor desc : descrs){
-					byte[] bs = desc.getValue();
-					Log.d(TAG, "Got descriptor UUID: "+desc.getUuid() +
-							" value: " + (bs!=null? new String():"null") +
-							" properties: " + charac.getProperties() +
-							" permissions: " + desc.getPermissions());
-				}
-			}
-		}
-	}
-
 	@Override
 	protected void onStop() {
 		super.onStop();
-
+		//When the application stops we disconnect
 		bluetoothAdapter.stopLeScan(handleScan);
 		if(bluetoothGatt != null)
 			bluetoothGatt.disconnect();
